da2bd82ab697620072d4efa912de7b5a1d5f1de9
YuryKarlianok <yury.karlianok@leverx.com>
2021-01-01 19:05:08 +0300

    Initial commit

---
+++ Pixels/Pixels/Pixels.cpp
@@ -0,0 +1,94 @@
+﻿#include <windows.h>
+#include <wchar.h>
+#include <time.h>
+
+//17. Заменить в рисунке красный цвет синим, а синий – красным (на рисунке должны быть и синий, и красный цвета)
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void DrawPixels(HWND hwnd);
+
+int WINAPI wWinMain(
+	HINSTANCE   hInstance,
+	HINSTANCE   hPrevInstance,
+	PWSTR       lpCmdLine,
+	int         nCmdShow
+) {
+
+	MSG  msg;
+	WNDCLASSW wc = { 0 };
+
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpszClassName = L"Pixels";
+	wc.hInstance = hInstance;
+	wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
+	wc.lpfnWndProc = WndProc;
+	wc.hCursor = LoadCursor(0, IDC_ARROW);
+
+	RegisterClassW(&wc);
+	CreateWindowW(wc.lpszClassName, L"Pixels",
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		100, 100, 300, 250, NULL, NULL, hInstance, NULL);
+
+	while (GetMessage(&msg, NULL, 0, 0)) {
+
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	srand(time(NULL));
+
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
+	WPARAM wParam, LPARAM lParam) {
+
+	switch (msg) {
+
+	case WM_PAINT:
+
+		DrawPixels(hwnd);
+		break;
+
+	case WM_DESTROY:
+
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProcW(hwnd, msg, wParam, lParam);
+}
+
+void DrawPixels(HWND hwnd) {
+
+	PAINTSTRUCT ps;
+	RECT r;
+
+	GetClientRect(hwnd, &r);
+
+	if (r.bottom == 0) {
+
+		return;
+	}
+
+	HDC hdc = BeginPaint(hwnd, &ps);
+
+	for (int i = 0; i < 3000; i++) {
+
+		int x = rand() % r.right;
+		int y = rand() % r.bottom;
+		SetPixel(hdc, x, y, RGB(255, 0, 0));
+	}
+
+	RECT blueRect;										//создаю прямоугольник
+	blueRect.left = (r.right - r.left) / 2;				//левая его сторона начинается с центра текущего экрана
+	blueRect.top = r.top;								//верх - такой же, как и у экрана
+	blueRect.right = r.right;							//правая сторона - такая же, как и у экрана
+	blueRect.bottom = r.bottom / 2;						//низ - до середины экрана
+
+	HBRUSH brush = CreateSolidBrush(RGB(0, 0, 255));	//задаю цвет - синий
+
+	FillRect(hdc, &blueRect, brush);					//рисую закрашенный синий прямоугольник со значениями краёв, указанными выше
+
+	EndPaint(hwnd, &ps);
+}
---
+++ SoProc/SoProc/SoProc.cpp
@@ -0,0 +1,62 @@
+﻿// SoProc.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdio.h>
+int main()
+{
+	float a, b, c;
+	float y = 0;
+	std::cout << " Vvedite chislo a: ";
+	std::cin >> a;
+	std::cout << " Vvedite chislo b: ";
+	std::cin >> b;
+	std::cout << " Vvedite chislo c: ";
+	std::cin >> c;
+	__asm {
+	START:
+		FINIT
+		FLD a
+		FLD b
+		FCOMI st(0), st(1)
+		jbe AleB
+		ja AgtB
+
+		AleB :
+		FSTP st(1)
+		FLD c
+		FCOMI st(0), st(1)
+		jbe AleC
+		ja AgtC
+
+		AleC :
+		FST y
+		JNE END
+
+		AgtC :
+		FSTP st(0)
+		FST y
+		JNE END
+
+		AgtB :
+		FSTP st(0)
+		FLD c
+		FCOMI st(0), st(1)
+		jbe BleC
+		ja BgtC
+
+		BleC :
+		FST y
+		JNE END
+
+		BgtC :
+		FSTP st(0)
+		FST y
+		JNE END
+
+		END:
+		FWAIT // Синхронизация работы с центральным процессором
+	}
+	std::cout << "\n Minimalnoe chislo y=min(a, b, c) = " << y;
+	std::cout << "\n By Borodina Elizaveta. FIT 3 course, 4 group \n";
+}
